e4081e9363c77e63ee4a68ca250b12240313474d
Andrej Buday <andrej.buday@gmail.com>
2018-03-24 15:39:38 +0100

    Reworked engine

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -32,80 +32,117 @@ export class PomodoroComponent implements OnInit {
       } else {
         this.funBreak();
       }
-    } else {
-      console.log('Do nothing');
     }
-
   }
 
   funWork() {
-    console.log(this);
-    // original
-    this.s = this.s + 1;
-    // test
-    // this.s = this.s + 60;
-    if ( this.s % 60 === 0) {
-      this.s = 0;
+    if (this.s > 0) {
+      this.s = this.s - 1;
       this.second = ('0' + this.s).slice(-2);
-      // original
-      this.m = this.m + 1;
-      // test
-      // this.m = this.m + 5;
-      this.minute = ('0' + this.m).slice(-2);
-      if (this.m % 2 === 0) {
-        this.s = 0;
-        this.m = 0;
-        this.second = '00';
-        this.minute = '00';
+    } else {
+      if (this.m > 0) {
+        this.s = 59;
+        this.m = this.m - 1;
+        this.second = ('0' + this.s).slice(-2);
+        this.minute = ('0' + this.m).slice(-2);
+      } else {
+        this.setToTest();
+        // this.resetMinutesTo5();
+        // this.resetMinuteTo00Seconds();
+        // console.log(this.s);
+        // console.log(this.m);
         this.timeToWork = false;
         this.timeForBreak = true;
         this.procrastinationTime = 'Look away from the screen!';
         this._document.getElementById('appFavicon').setAttribute('href', '/assets/icon/green.ico');
       }
     }
-    this.second = ('0' + this.s).slice(-2);
   }
 
   funBreak() {
-    console.log('Look Away');
-    // original
-    this.s = this.s + 1;
-    // test
-    // this.s = this.s + 60;
-    if ( this.s % 60 === 0) {
-      this.s = 0;
+    if (this.s > 0) {
+      this.s = this.s - 1;
       this.second = ('0' + this.s).slice(-2);
-      // original
-      this.m = this.m + 1;
-      // test
-      // this.m = this.m + 5;
-      this.minute = ('0' + this.m).slice(-2);
-      if (this.m % 5 === 0) {
-        this.s = 0;
+    } else {
+      if (this.m > 0) {
+        this.s = 59;
+        this.m = this.m - 1;
         this.second = ('0' + this.s).slice(-2);
-        this.s = 0;
-        this.m = 0;
-        this.second = '00';
-        this.minute = '00';
+        this.minute = ('0' + this.m).slice(-2);
+      } else {
+        this.setToTest();
+        this.resetMinutesTo25();
+        this.resetMinuteTo00Seconds();
         this.timeToWork = true;
         this.timeForBreak = false;
-        this.procrastinationTime = '';
-        this._document.getElementById('appFavicon').setAttribute('href', '/assets/icon/blue.ico');
+        this.procrastinationTime = 'Look away from the screen!';
+        this._document.getElementById('appFavicon').setAttribute('href', '/assets/icon/green.ico');
       }
     }
-    this.second = ('0' + this.s).slice(-2);
+    // console.log('Look Away');
+    // // original
+    // this.s = this.s + 1;
+    // // test
+    // // this.s = this.s + 60;
+    // if ( this.s % 60 === 0) {
+    //   this.s = 0;
+    //   this.second = ('0' + this.s).slice(-2);
+    //   // original
+    //   this.m = this.m + 1;
+    //   // test
+    //   // this.m = this.m + 5;
+    //   this.minute = ('0' + this.m).slice(-2);
+    //   if (this.m % 5 === 0) {
+    //     this.s = 0;
+    //     this.second = ('0' + this.s).slice(-2);
+    //     this.s = 0;
+    //     this.m = 0;
+    //     this.second = '00';
+    //     this.minute = '00';
+    //     this.timeToWork = true;
+    //     this.timeForBreak = false;
+    //     this.procrastinationTime = '';
+    //     this._document.getElementById('appFavicon').setAttribute('href', '/assets/icon/blue.ico');
+    //   }
+    // }
+    // this.second = ('0' + this.s).slice(-2);
   }
 
   resetClock() {
-    this.s = 0;
-    this.m = 0;
-    this.minute = '00';
-    this.second = '00';
+    this.setTo25();
     this.timeToWork = true;
     this.timeForBreak = false;
   }
 
+  resetMinutesTo5() {
+    this.m = 5;
+    this.minute = '05';
+  }
+
+  resetMinutesTo25() {
+    this.m = 25;
+    this.minute = '25';
+  }
+
+  resetMinuteTo00Seconds() {
+    this.s = 0;
+    this.second = '00';
+  }
+
+  setTo25() {
+    this.resetMinutesTo25();
+    this.resetMinuteTo00Seconds();
+  }
+
+  setToTest() {
+    this.m = 0;
+    this.minute = '00';
+    this.s = 2;
+    this.second = '02';
+  }
+
   startPomodoro() {
+    this.resetClock()
     this.run = true;
   }
 
